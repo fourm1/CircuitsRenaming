1e18167327d3ef43f4bfb177f70d008deaaa2bb4
fourm1 <toto.63146@gmail.com>
2025-01-03 02:21:57 +0100

    Initial commit

---
+++ src/CircuitsRenaming/metadata.lua
@@ -0,0 +1,11 @@
+return PlaceObj('ModDef', {
+	'title', "CircuitsRenaming",
+	'description', "Give ability to have infinite electric circuits and rename them",
+	'id', "EF7K7Ne",
+	'author', "Hubert Punisseur de la BAC",
+	'version', 2,
+	'lua_revision', 233360,
+	'saved_with_revision', 352677,
+	'saved', 1735864680,
+	'TagGameplay', true,
+})
